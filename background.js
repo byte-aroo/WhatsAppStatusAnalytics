4cd5a03c98135de6218490ca09bca5f8a8f5cd34
Akshat Jain <akshat.jain@aurigait.com>
2024-07-11 09:53:43 +0530

    Upgraded to manifest version 3

--- background.js
+++ background.js
@@ -1,17 +1,51 @@
 var toggle = false;
-var statuss = 'on';
-var the_tab_id = '';
+var statuss = "on";
+var the_tab_id = "";
 
+function toggle_extension(tab) {
+  // Set icon
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: tab.id },
+      func: (statuss) => {
+        window.extension_statuss = statuss;
+      },
+      args: [statuss],
+    },
+    () => {
+      if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError);
+      }
+    }
+  );
 
-function toggle_extension(tab){
-    // Set icon
-    chrome.tabs.executeScript({ code: 'var extension_statuss = "'+statuss+'"' });
-    chrome.tabs.executeScript({ file: 'html2Canvas.js'})
-    chrome.tabs.executeScript({ file: 'inject.js' });
-    the_tab_id = tab.id;
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: tab.id },
+      files: ["html2Canvas.js"],
+    },
+    () => {
+      if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError);
+      }
+    }
+  );
+
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: tab.id },
+      files: ["inject.js"],
+    },
+    () => {
+      if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError);
+      }
+    }
+  );
+
+  the_tab_id = tab.id;
 }
 
-
-chrome.browserAction.onClicked.addListener(function(tab) {
-    toggle_extension(tab);
+chrome.action.onClicked.addListener((tab) => {
+  toggle_extension(tab);
 });
